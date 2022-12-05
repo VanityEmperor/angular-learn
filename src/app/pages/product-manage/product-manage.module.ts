52cbbb48c53019180fee55365109230b1eea715a
姬虚空 <2765812425@qq.com>
2022-12-05 22:25:49 +0800

    添加product-detail组件

--- src/app/pages/product-manage/product-manage.module.ts
+++ src/app/pages/product-manage/product-manage.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductManageComponent } from './product-manage.component';
 import { ProductManageRoutes } from './product-manage.routing';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductManageRoutes
   ],
-  declarations: [ProductManageComponent,ProductListComponent]
+  declarations: [ProductManageComponent,ProductListComponent,ProductDetailComponent]
 })
 export class ProductManageModule { }
--- src/app/pages/product-manage/product-manage.routing.ts
+++ src/app/pages/product-manage/product-manage.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ProductManageComponent } from './product-manage.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 const routes: Routes = [
   { path: '', component: ProductManageComponent,},
   { path: 'productList', component: ProductListComponent,},
+  { path: 'productDetail', component: ProductDetailComponent,},
 ];
 
 export const ProductManageRoutes = RouterModule.forChild(routes);
