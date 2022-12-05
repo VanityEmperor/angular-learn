fd802d6432f0954972891b7b0d9aa3296e1013d8
姬虚空 <2765812425@qq.com>
2022-12-05 22:04:01 +0800

    添加third组件

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutes } from './pages.routing';
+import { ThirdComponent } from './third/third.component';
 @NgModule({
   imports: [
     CommonModule,
     PagesRoutes
   ],
-  declarations: [PagesComponent]
+  declarations: [PagesComponent,ThirdComponent]
 })
 export class PagesModule { }
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,9 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './pages.component';
+import { ThirdComponent } from './third/third.component';
 const routes: Routes = [
   {  
     path: '',
     component: PagesComponent,
+  },
+  {  
+    path: 'third',
+    component: ThirdComponent,
   },
 ];
 
