3b7ac04c53b185a549c014b15bffeebcd4a8972e
姬虚空 <2765812425@qq.com>
2022-12-05 22:24:11 +0800

    添加product-list组件

--- src/app/pages/product-manage/product-manage.module.ts
+++ src/app/pages/product-manage/product-manage.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductManageComponent } from './product-manage.component';
 import { ProductManageRoutes } from './product-manage.routing';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductManageRoutes
   ],
-  declarations: [ProductManageComponent]
+  declarations: [ProductManageComponent,ProductListComponent]
 })
 export class ProductManageModule { }
--- src/app/pages/product-manage/product-manage.routing.ts
+++ src/app/pages/product-manage/product-manage.routing.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ProductManageComponent } from './product-manage.component';
+import { ProductListComponent } from './product-list/product-list.component';
 const routes: Routes = [
   { path: '', component: ProductManageComponent,},
+  { path: 'productList', component: ProductListComponent,},
 ];
 
 export const ProductManageRoutes = RouterModule.forChild(routes);
