aca9c1028d1f851cef8b0f26bc313df838820387
姬虚空 <2765812425@qq.com>
2022-12-05 21:54:17 +0800

    添加second组件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
   { path: 'helloWorld', component: HelloWorldComponent },
+  { path: 'second', component: SecondComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HelloWorldComponent
+      HelloWorldComponent,
+      SecondComponent
    ],
   imports: [
     BrowserModule,
