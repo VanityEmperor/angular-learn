408b3c05f2a678c260db3248c25a99bd202985cb
姬虚空 <2765812425@qq.com>
2022-12-05 21:48:51 +0800

    添加hello-world组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      HelloWorldComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
